754b1d351052aca95b68c29849960b8e7f35c6ca
FayedAhmed <fayedahmed1997@gmail.com>
2025-02-22 12:57:36 +0000

    conflicts resolved

--- test_packages.R
+++ test_packages.R
@@ -1,7 +1,3 @@
 library(RPostgres)
-<<<<<<< HEAD
-library(tidyverse)#often used in data science 
+library(tidyverse)#often used in data science load many packages 
 
-=======
-library(tidyverse) #load many packages 
->>>>>>> 6313bc6f422943c56acc090e141e6fa9c2d5bf46
